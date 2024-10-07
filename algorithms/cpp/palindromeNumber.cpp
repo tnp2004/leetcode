325378c034db2eded414c7805a2da66e82537ccd
tnp2004 <iceandiceisice2004@gmail.com>
2024-10-07 22:34:48 +0700

    C++: palindrome number

---
+++ algorithms/cpp/palindromeNumber.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        if (x >= 0 && x < 10) return true;
+        if (x % 10 == 0) return false;
+        int v = 0;
+        int c = x;
+
+        while(c > v) {
+            v *= 10;
+            v += c % 10;
+            c = c / 10;
+        }
+
+        if ((c == v) || c == (v / 10)) {
+            return true;
+        }
+
+        return false;
+    }
+};
+
+int main() {
+    Solution s;
+    bool r = s.isPalindrome(1234567899);
+    cout << "anwser: " << r;
+}
