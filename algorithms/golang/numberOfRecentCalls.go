6a8f3c5f1792a2549f316b3400ae6bc21b873710
tnp2004 <iceandiceisice2004@gmail.com>
2024-10-14 13:57:39 +0700

    Go: refactor number of recent calls

--- algorithms/golang/numberOfRecentCalls.go
+++ algorithms/golang/numberOfRecentCalls.go
@@ -12,8 +12,9 @@ func Constructor() RecentCounter {
 
 func (r *RecentCounter) Ping(t int) int {
 	r.queue = append(r.queue, t)
-	if len(r.queue) > 1 && t > 3000 {
-		for i := len(r.queue) - 2; i >= 0; i-- {
+	queueLen := len(r.queue)
+	if queueLen > 1 && t > 3000 {
+		for i := queueLen - 2; i >= 0; i-- {
 			if t-r.queue[i] > 3000 {
 				r.queue = append(r.queue[i+1:])
 				break
